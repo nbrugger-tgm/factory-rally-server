649e2b75c2a01aa03327839a828398049b679c8f
Nils Brugger <nbrugger@student.tgm.ac.at>
2020-03-18 21:53:45 +0100

    Create API Testing Project

---
+++ Test/Tgm.Roborally.Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Tgm.Roborally.Api.Api;
+using Tgm.Roborally.Api.Client;
+using Tgm.Roborally.Api.Model;
+
+namespace Tgm.Roborally.Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Configuration c = new Configuration {BasePath = "localhost:5050/v1"};
+			GameApi api = new GameApi();
+			GameRules r = new GameRules();
+			r.Name = "Test";
+			r.MaxPlayers = 4;
+			r.PlayerNamesVisible = true;
+			
+			Console.WriteLine("Create Games : ");
+			
+			api.CreateGame(r);
+			api.CreateGame(new GameRules() {Name = "Test1", MaxPlayers = 4, PlayerNamesVisible = true});
+			api.CreateGame(new GameRules() {Name = "Test2", MaxPlayers = 10, PlayerNamesVisible = true});
+
+			List<int> games = api.GetGames();
+			
+			Console.WriteLine(games.Count == 3 ? "Success" : "Fail");
+			
+			
+			Console.WriteLine("Change Game State (Pause) : ");
+			
+			Console.WriteLine("  Bevore : "+api.GetGameState(games[0]).State);
+			api.CommitAction(games[0],ActionType.PAUSE);
+			Console.WriteLine("  After Change : "+api.GetGameState(games[0]).State);
+			Console.WriteLine(api.GetGameState(games[0]).State == GameState.BREAK ? "Success" : "Fail");
+
+
+
+			Console.WriteLine("Unpause Game ");
+			Console.WriteLine("  Bevore : "+api.GetGameState(games[0]).State);
+			api.CommitAction(games[0],ActionType.UNPAUSE);
+			Console.WriteLine("  After Change : "+api.GetGameState(games[0]).State);
+			Console.WriteLine(api.GetGameState(games[0]).State == GameState.LOBBY ? "Success" : "Fail");
+		}
+	}
+}
