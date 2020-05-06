46d961e87cb5728bcde94353e6a7d6cc7cc1e400
NitonFx <nbrugger@student.tgm.ac.at>
2020-05-06 07:49:37 +0200

    Fix export script

--- client/export.bat
+++ client/export.bat
@@ -5,6 +5,6 @@ dotnet build
 dotnet pack
 "D:\Programme\JetBrains\apps\Rider\ch-0\193.6494.48\tools\MSBuild\Current\Bin\MSBuild.exe"
 del "C:\Program Files (x86)\Microsoft SDKs\NuGetPackages\Tgm.Roborally.Api" /F /S /Q
-nuget add .\bin\Debug\Tgm.Roborally.Api.1.0.0.nupkg -Source "C:\Program Files (x86)\Microsoft SDKs\NuGetPackages"
+nuget init bin\Debug "C:\Program Files (x86)\Microsoft SDKs\NuGetPackages"
 pause
 wait
