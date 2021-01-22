aa85c3732edd4c3283ea49505d88370788f44210
Nils Brugger <nbrugger@student.tgm.ac.at>
2021-01-22 13:38:28 +0100

    feat: implement robot picking/assignment

--- server/src/Tgm.Roborally.Server/Engine/Managers/EntityManager.cs
+++ server/src/Tgm.Roborally.Server/Engine/Managers/EntityManager.cs
@@ -1,7 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tgm.Roborally.Server.Models;
+
 namespace Tgm.Roborally.Server.Engine
 {
-	public class EntityManager
-	{
-		
+	public class EntityManager {
+		private readonly List<Entity> ents = new List<Entity>();
+		public RobotPickEvent PickRobo(Robots type, Player gamePlayer) {
+			RobotInfo info = new RobotInfo() {
+				Health = 100,
+				Id     = NextFreeId()
+			};
+			info.Name = gamePlayer.DisplayName +" "+ info.Id;
+			ents.Add(info);
+			gamePlayer.ControlledEntities.Add(info.Id);
+			RobotPickEvent ev = new RobotPickEvent() {
+				Player = gamePlayer.Id,
+				Robot  = info.Id
+			};
+			return ev;
+		}
+
+		private static readonly Random Rng = new Random();
+
+		private int NextFreeId() {
+			int r;
+			
+			do {
+				r = Rng.Next(15);
+			} while (ents.Select(e => e.Id).Contains(r));
+
+			return r;
+		}
 	}
 }
--- server/src/Tgm.Roborally.Server/Engine/Phases/RobotPickingPhase.cs
+++ server/src/Tgm.Roborally.Server/Engine/Phases/RobotPickingPhase.cs
@@ -1,12 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Tgm.Roborally.Server.Models;
 
 namespace Tgm.Roborally.Server.Engine.Phases
 {
 	public class RobotPickingPhase : GamePhase
 	{
-		protected override GamePhase Run(GameLogic game)
-		{
-			//TODO Later on maybe a more intelligent algorithm will take place here
+		protected override GamePhase Run(GameLogic game) {
+			//TODO: Later on maybe a more intelligent algorithm will take place here
+			
+			List<Robots> available = Enum.GetValues(typeof(Robots)).Cast<Robots>().ToList();
+			foreach (Player gamePlayer in game.Players) {
+				Robots type = available[0];
+				Event e = game.Entitys.PickRobo(type,gamePlayer);
+				game.CommitEvent(e);
+				available.Remove(type);
+			}
+
 			return new MainPhase();
 		}
 
