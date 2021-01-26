1d556a6a3f0d3af09307c35fd4285a9e17d9f797
Nils Brugger <nbrugger@student.tgm.ac.at>
2021-01-26 11:35:35 +0100

    feat: add info to BadEventException

--- server/src/Tgm.Roborally.Server/Engine/Exceptions/BadEventException.cs
+++ server/src/Tgm.Roborally.Server/Engine/Exceptions/BadEventException.cs
@@ -2,8 +2,8 @@ using System;
 
 namespace Tgm.Roborally.Server.Engine.Exceptions {
 	public class BadEventException : Exception {
-		public BadEventException(string ev_type) : base(
-			"The event/action " + ev_type + " is not allowed in this state ") {
+		public BadEventException(string ev_type, string? fullName) : base(
+			"The event/action " + ev_type + " is not allowed in this state "+fullName) {
 		}
 	}
 }
