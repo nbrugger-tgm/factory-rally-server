ee7badeab34aadd51d25173956bb0a645656ee22
nbrugger-tgm <nbrugger@student.tgm.ac.at>
2021-02-03 17:05:47 +0100

    test: expand testcases

--- Test/Tgm.Roborally.Test/Program.cs
+++ Test/Tgm.Roborally.Test/Program.cs
@@ -9,16 +9,19 @@ namespace Tgm.Roborally.Test {
 			Configuration c       = new Configuration { BasePath = "http://localhost:5050/v1" };
 			GameApi       api     = new GameApi(c);
 			PlayersApi    players = new PlayersApi(c); 
-			GameRules     rules   = new GameRules(true, 4, "Test", 1);
+			GameRules     rules   = new GameRules(true, 4, "Test", 1,password:null);
 			
 			int game = api.CreateGame(rules);
 			Console.Out.WriteLine(game);
-
-			JoinResponse response = players.Join(game);
-			
-			Console.Out.WriteLine(response);
 			Console.Out.WriteLine(api.GetGameState(game));
-
+			Console.Out.WriteLine(players.Join(gameId:game));
+			JoinResponse response = players.Join(game);
+			Console.Out.WriteLine($"joint response : {response}");
+			Console.Out.WriteLine($"id : {response.Id}");
+			Console.Out.WriteLine($"pat : {response.Pat}");
+			c.ApiKey["pat"] = response.Pat;
+			Console.Out.WriteLine(response);
+			Console.Out.WriteLine(players.GetPlayer(game,response.Id));
 		}
 	}
 }
