c60e81ea4b5daf4b6eccea5eb301347c4e54f6eb
Nils Brugger <nbrugger@student.tgm.ac.at>
2021-02-03 04:02:11 +0100

    build(client): update autogen config & scripts

--- autogen-client.bat
+++ autogen-client.bat
@@ -1,1 +1,1 @@
-openapi-generator generate -c autogen-config/csharp.json -g csharp -i oas/game-engine.v1.json -o client
+openapi-generator generate -c autogen-config/csharp-client.json -g csharp-netcore -i oas/game-engine.v1.json -o client
