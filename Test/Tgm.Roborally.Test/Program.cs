eb46386484ab2d7cfccc8eace86bb8f086fdd597
Nils Brugger <nbrugger@student.tgm.ac.at>
2021-02-08 09:55:36 +0100

    test: add consumer testcase

--- Test/Tgm.Roborally.Test/Program.cs
+++ Test/Tgm.Roborally.Test/Program.cs
@@ -10,7 +10,8 @@ namespace Tgm.Roborally.Test {
 			GameApi       api     = new GameApi(c);
 			PlayersApi    players = new PlayersApi(c);
 
-			TestStartGame(api, players, c);
+			TestStartGame(api,players,c);
+			TestConsumer(api, players, c);
 		}
 
 		private static void TestStartGame(GameApi api, PlayersApi players, Configuration config) {
@@ -35,6 +36,34 @@ namespace Tgm.Roborally.Test {
 			PrintHeader("Fetch Events");
 			EventHandlingApi eventApi = new EventHandlingApi(config);
 			for (int i = 0; i < 8; i++) {
+				Print(eventApi.FetchNextEvent(game).Type.ToString());
+			}
+		}
+		
+		private static void TestConsumer(GameApi api, PlayersApi players, Configuration config) {
+			GameRules rules = new GameRules(true, 2, "Test", 1, password: null);
+
+			PrintHeader("Create Game");
+			int game = api.CreateGame(rules);
+			Print("Game ID: " + game);
+			Print("Stats : "  + api.GetGameState(game));
+			PrintHeader("Join Game");
+			players.Join(game);
+			ConsumerApi consumerApi = new ConsumerApi(config);
+			JoinResponse response = consumerApi.RegisterConsumer(game,new ConsumerRegistration("Test","Ein consumer"));
+			Print($"id : {response.Id}");
+			Print($"pat : {response.Pat}");
+			players.Configuration.ApiKey["pat"] = response.Pat;
+			api.Configuration.ApiKey["pat"]     = response.Pat;
+			config.ApiKey["pat"]                = response.Pat;
+			JoinResponse somePlayer = players.Join(game);
+
+			Print(players.GetPlayer(game, somePlayer.Id).ToJson());
+			PrintHeader("Start Game");
+			api.CommitAction(game, ActionType.STARTGAME);
+			PrintHeader("Fetch Events");
+			EventHandlingApi eventApi = new EventHandlingApi(config);
+			for (int i = 0; i < 9; i++) {
 				Print(eventApi.FetchNextEvent(game).Type.ToString());
 			}
 		}
