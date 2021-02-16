aeb6539995274ac2ded99a0fe5b90d4fd826d053
Nils Brugger <nbrugger@student.tgm.ac.at>
2021-02-16 12:27:05 +0100

    feat: add "DeckEmpty" Exception

--- server/src/Tgm.Roborally.Server/Engine/Exceptions/GameFlowException.cs
+++ server/src/Tgm.Roborally.Server/Engine/Exceptions/GameFlowException.cs
@@ -2,7 +2,8 @@ using System;
 
 namespace Tgm.Roborally.Server.Engine.Phases {
 	public class GameFlowException : Exception {
-		public const string BAD_EVENT = "The game died due to an illegal event";
+		public const string NO_DECK = "There are no discarded cards and the Deck is empty";
+		public const  string BAD_EVENT = "The game died due to an illegal event";
 
 		public GameFlowException(String badEvent) : base(badEvent) {
 		}
