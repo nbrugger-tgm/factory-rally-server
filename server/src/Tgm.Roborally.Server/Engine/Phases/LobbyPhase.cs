a1515c5086612a3a0d5e907235b614706a79cd96
Nils Brugger <nbrugger@student.tgm.ac.at>
2021-02-28 22:52:20 +0100

    perf: remove active yield waiting (-98% CPU usage)

--- server/src/Tgm.Roborally.Server/Engine/Phases/LobbyPhase.cs
+++ server/src/Tgm.Roborally.Server/Engine/Phases/LobbyPhase.cs
@@ -4,19 +4,26 @@ using Tgm.Roborally.Server.Models;
 
 namespace Tgm.Roborally.Server.Engine.Phases {
 	public class LobbyPhase : GamePhase {
-		private bool started;
+		private object started = new object();
 
 		protected override object Information => null;
 
 		public override GameState NewState => GameState.LOBBY;
 
 		protected override GamePhase Run(GameLogic game) {
-			while (!started) Thread.Yield();
+			lock (started) {
+				Monitor.Wait(started);
+			}
 
 			return new RobotPickingPhase();
 		}
 
-		public override void Notify(ActionType action) => started = action == ActionType.STARTGAME;
+		public override void Notify(ActionType action) {
+			if (action == ActionType.STARTGAME)
+				lock (started) {
+					Monitor.Pulse(started);
+				}
+		}
 
 		public override bool Notify(GenericEvent action) => action.Data is JoinEvent;
 
