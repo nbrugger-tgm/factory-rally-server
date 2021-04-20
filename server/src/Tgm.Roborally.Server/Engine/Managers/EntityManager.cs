dd438287091df04bccf6aebe752d70fce4397283
Nils Brugger <nbrugger@student.tgm.ac.at>
2021-04-21 00:25:44 +0200

    fix: bad robot selection (chrash)

--- server/src/Tgm.Roborally.Server/Engine/Managers/EntityManager.cs
+++ server/src/Tgm.Roborally.Server/Engine/Managers/EntityManager.cs
@@ -67,7 +67,7 @@ namespace Tgm.Roborally.Server.Engine.Managers {
 		public void PlaceRobotsOnSpawn() {
 			for (int i = 0; i < Robots.Count; i++) {
 				Position spawn = _game.Map.Spawns[i];
-				_ents[Robots[i]].Location = spawn;
+				this[Robots[i]].Location = spawn;
 			}
 		}
 
