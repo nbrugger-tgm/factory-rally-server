e562d902a3fbadfe1943cc9cd2ce7a79c6e2745f
Nils Brugger <nbrugger@student.tgm.ac.at>
2021-04-24 16:17:45 +0200

    docs: add missing XML comment

--- server/src/Tgm.Roborally.Server/Engine/Statement/RobotCommandExecutor.cs
+++ server/src/Tgm.Roborally.Server/Engine/Statement/RobotCommandExecutor.cs
@@ -1,4 +1,7 @@
 namespace Tgm.Roborally.Server.Engine.Statement {
+	/// <summary>
+	/// The logic of a command, implement this interface to implement a new command
+	/// </summary>
 	public interface RobotCommandExecutor {
 		/// <summary>
 		///     Do whatever shuld happen when this card is executed
